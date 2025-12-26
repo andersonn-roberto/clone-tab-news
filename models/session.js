7508091f8901e0a40371c5c4d91ee5f54ddec408
Anderson Oliveira <andersonn.roberto@gmail.com>
2025-12-26 00:50:52 +0000

    feat: implement `/api/v1/user` endpoint with session renewal

--- models/session.js
+++ models/session.js
@@ -1,11 +1,45 @@
 import crypto from "node:crypto";
 import database from "infra/database";
+import { UnauthorizedError } from "infra/errors";
 
-const EXPIRATION_IN_MILISECONDS = 60 * 60 * 24 * 30 * 1000; //30 days
+const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; //30 days
+
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await runSelectQUery(sessionToken);
+
+  return sessionFound;
+
+  async function runSelectQUery(sessionToken) {
+    const results = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        sessions
+      WHERE
+        token = $1
+      AND
+        expires_at >= now()
+      LIMIT
+        1
+    `,
+      values: [sessionToken],
+    });
+
+    if (results.rowCount === 0) {
+      throw new UnauthorizedError({
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userId) {
   const token = crypto.randomBytes(48).toString("hex");
-  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILISECONDS);
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
 
   const newSession = await runInsertQuery(token, userId, expiresAt);
   return newSession;
@@ -13,13 +47,13 @@ async function create(userId) {
   async function runInsertQuery(token, userId, expiresAt) {
     const results = await database.query({
       text: `
-              INSERT INTO
-                sessions (token, user_id, expires_at)
-              VALUES
-                ($1, $2, $3)
-              RETURNING
-                *
-            `,
+        INSERT INTO
+          sessions (token, user_id, expires_at)
+        VALUES
+          ($1, $2, $3)
+        RETURNING
+          *
+      `,
       values: [token, userId, expiresAt],
     });
 
@@ -27,9 +61,37 @@ async function create(userId) {
   }
 }
 
+async function renew(sessionId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const renewedSessionObject = runUpdateQuery(sessionId, expiresAt);
+  return renewedSessionObject;
+
+  async function runUpdateQuery(sessionId, expiresAt) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = $2,
+          updated_at = now()
+        WHERE
+          id = $1
+        RETURNING
+          *
+      `,
+      values: [sessionId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
+  findOneValidByToken,
   create,
-  EXPIRATION_IN_MILISECONDS,
+  renew,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default session;
