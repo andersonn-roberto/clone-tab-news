7d17147509625aae86eaf7fd024284345fc145ac
Anderson Oliveira <andersonn.roberto@gmail.com>
2025-12-27 14:16:22 +0000

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -1,16 +1,27 @@
 import email from "infra/email";
 import database from "infra/database";
 import webserver from "infra/webserver";
-import { NotFoundError } from "infra/errors";
+import { ForbiddenError, NotFoundError } from "infra/errors";
 import user from "./user";
+import authorization from "./authorization";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; //15 minutes
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
   ]);
+
   return activatedUser;
 }
 
@@ -113,6 +124,7 @@ const activation = {
   findOneValidById,
   create,
   sendEmailToUser,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
